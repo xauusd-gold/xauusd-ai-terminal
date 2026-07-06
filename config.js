9ad8162bce97e71197462c0fd213795053dd94c7
xauusd-gold <mini.shakil.hmalik@gmail.com>
2026-07-06 13:35:44 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const CONFIG = {
     // TwelveData API Key
-    TWELVEDATA_API_KEY: "YOUR_TWELVEDATA_API_KEY",
+    TWELVEDATA_API_KEY: "521a7e5df242437c8690884978286550",
 
     // Finnhub API Key
-    FINNHUB_API_KEY: "YOUR_FINNHUB_API_KEY",
+    FINNHUB_API_KEY: "d95lus1r01qqtvdmadcgd95lus1r01qqtvdmadd0",
 
     // Timezone
     TIMEZONE: "Asia/Kolkata",
